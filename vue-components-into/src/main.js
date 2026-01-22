68d72ed7d49ba15ad68194c879aafe6accf5918a
nunorulz <nunorulz@gmail.com>
2026-01-22 16:46:08 -0300

    Registrando componentes globalmente

--- vue-components-into/src/main.js
+++ vue-components-into/src/main.js
@@ -1,14 +1,57 @@
 import Vue from 'vue'
 
-Vue.component('br-component', {
-    data: function() {
+/*Vue.component('br-component', {
+    data() {
         return {
-            titulo: "Curso de Vue Js"
+            titulo: "Curso de Vue Js",
+            contador: 0
         }
     },
-    template: '<h1>{{titulo}}</h1>'
+    methods: {
+        incrementar() {
+            this.contador++
+        }
+    },
+    template: `
+    <div>
+      <h1>{{ titulo }}</h1>
+      <button @click="incrementar">
+        Clicado {{ contador }} vezes
+      </button>
+    </div>
+  `
+})*/
+
+const brComponent = {
+
+    data() {
+        return {
+            titulo: "Curso de Vue Js",
+            contador: 0
+        }
+    },
+    methods: {
+        incrementar() {
+            this.contador++
+        }
+    },
+    template: `
+    <div>
+      <h1>{{ titulo }}</h1>
+      <button @click="incrementar">
+        Clicado {{ contador }} vezes
+      </button>
+    </div>
+  `
+}
+
+new Vue({
+    el: '#app1',
+    components: {
+        'br-component': brComponent
+    }
 })
 
 new Vue({
-    el: '#app',
+    el: '#app2'
 })
