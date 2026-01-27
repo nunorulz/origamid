5ab3c6f138148be00c2181c4ad1ef57daa887a2d
nunorulz <nunorulz@gmail.com>
2026-01-27 17:35:01 -0300

    Adicionado pasta de exercicios

--- vue-components-comunicacao/src/App.vue
+++ vue-components-comunicacao/src/App.vue
@@ -2,13 +2,22 @@
   
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      
+      <h1 class="display-4">Lista de Filmes</h1>
+      <p class="lead">Treiando comunicação entre Componentes no VUE</p>
     </div>
+  </div>
 
+  <div class="container">
+    <FilmeLista/>
   </div>
 
 </template>
 
 <script>
-
+import FilmeLista from "./components/FilmeLista.vue";
+export default {
+  components: {
+    FilmeLista
+  }
+}
 </script>
