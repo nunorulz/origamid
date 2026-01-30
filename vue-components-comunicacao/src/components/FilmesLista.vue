7c41481a57cbcef8cfb4dcb2d55ce7099e91de9d
nunorulz <nunorulz@gmail.com>
2026-01-30 15:58:27 -0300

    Estudo props

--- vue-components-comunicacao/src/components/FilmesLista.vue
+++ vue-components-comunicacao/src/components/FilmesLista.vue
@@ -2,11 +2,9 @@
   <div class="row">
     <div class="col-8">
       <h2>Filmes</h2>
-      <div class="list-group list-group-flush">
-        <FilmesListaIten filmeTitulo="Vingadores: Guerra Infinita"/>
-        <FilmesListaIten filmeTitulo="Homem Formiga e a Vespa"/>
-        <FilmesListaIten filmeTitulo="Pantera Negra"/>
-      </div>
+      <ul class="list-group list-group-flush">
+        <FilmesListaIten v-for="filme in filmes" :key="filme.id" :filmeTitulo="filme.titulo"/>
+      </ul>
     </div>
     <div class="col-4">
       <FilmesListaItenInfo/>
@@ -20,7 +18,17 @@ import FilmesListaItenInfo from "@/components/FilmesListaItenInfo.vue";
 export default {
   components: {
     FilmesListaIten,
-    FilmesListaItenInfo,
+    FilmesListaItenInfo
+  },
+  data(){
+    return {
+      filmes:[
+        {id:1, titulo: 'Vingadores: Guerra Infinita'},
+        {id:2, titulo: 'Homem Formiga e a Vespa'},
+        {id:3, titulo: 'Pantera Negra'},
+        {id:4, titulo: 'DeadPool 2'},
+      ]
+    }
   }
 }
 </script>
--- vue-components-comunicacao/src/components/FilmesListaIten.vue
+++ vue-components-comunicacao/src/components/FilmesListaIten.vue
@@ -1,14 +1,31 @@
 <template>
   <li class="list-group-item">
     <div class="d-flex justify-content-between">
-    <span>{{ filmeTitulo }}</span>
-    <button class="btn btn-secondary ">Selecionar</button>
+    <span>{{ filmeTituloConcatenado }}</span>
+    <button class="btn btn-secondary">Selecionar</button>
     </div>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['filmeTitulo']
+  props: {
+    filmeTitulo:{
+    type: String,
+      required: true
+    },
+    filmeAno:{
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    filmeTituloConcatenado(){
+      return `Título: ${this.filmeTitulo}`
+    }
+  },
+  created(){
+    console.log(typeof this.filmeTitulo)
+  }
 }
 </script>
